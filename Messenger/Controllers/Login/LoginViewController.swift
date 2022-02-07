b595a9d1c6230f505643b88fd46a889dcebd0b29
Dmitry <Atlantiskun@gmail.com>
2022-02-07 14:10:34 +0700

    NEW: show message's history; update latest message on screen; can see new messages in real time

--- Messenger/Controllers/Login/LoginViewController.swift
+++ Messenger/Controllers/Login/LoginViewController.swift
@@ -174,6 +174,23 @@ class LoginViewController: UIViewController {
             }
             
             let user = result.user
+            
+            let safeEmail = DatabaseManager.safeEmail(emailAddress: email)
+            DatabaseManager.shared.getDataFor(path: safeEmail) { result in
+                switch result {
+                case .success(let data):
+                    guard let userData = data as? [String: Any],
+                          let firstName = userData["first_name"] as? String,
+                          let lastName = userData["last_name"] as? String else {
+                        return
+                    }
+                    UserDefaults.standard.set("\(firstName) \(lastName)", forKey: "name")
+                    
+                case .failure(let error):
+                    print("Failed ot read data with error \(error)")
+                }
+            }
+            
             UserDefaults.standard.set(email, forKey: "email")
             
             print("Logged in User: \(user)")
@@ -246,6 +263,7 @@ extension LoginViewController: LoginButtonDelegate {
             }
             
             UserDefaults.standard.set(email, forKey: "email")
+            UserDefaults.standard.set("\(firstName) \(lastName)", forKey: "name")
             
             DatabaseManager.shared.userExists(with: email) { exists in
                 if !exists {
@@ -325,6 +343,7 @@ extension LoginViewController {
                let emailAddres = user.profile?.email {
                 
                 UserDefaults.standard.set(emailAddres, forKey: "email")
+                UserDefaults.standard.set("\(givenName) \(familyName)", forKey: "name")
                 
                 DatabaseManager.shared.userExists(with: emailAddres) { exists in
                     if !exists {
